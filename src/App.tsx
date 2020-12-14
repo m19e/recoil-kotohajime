25bf993584c87b7e1c831319a259145fa41d60d2
m19e <nexus.me7290@gmail.com>
2020-12-15 05:27:38 +0900

    Load todolist on app

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { RecoilRoot } from "recoil";
 import CharacterCounter from "./components/CharacterCounter";
+import TodoList from "./components/TodoList";
 
 export default function App() {
     return (
@@ -8,7 +9,8 @@ export default function App() {
             <div style={{ width: "100%", height: "100vh", backgroundColor: "#1DA1F2" }}>
                 <div style={{ display: "flex", flex: 1, flexDirection: "column" }}>
                     <h1 style={{ textAlign: "center", fontSize: "6rem", color: "white" }}>Recoil</h1>
-                    <CharacterCounter />
+                    <TodoList />
+                    {/* <CharacterCounter /> */}
                 </div>
             </div>
         </RecoilRoot>
