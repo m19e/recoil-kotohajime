882a35811d8ea57b285312c6af9ea961e012b893
m19e <nexus.me7290@gmail.com>
2020-12-15 03:30:27 +0900

    Add echo textinput

--- src/components/CharacterCounter.tsx
+++ src/components/CharacterCounter.tsx
@@ -1,11 +1,33 @@
 import React from "react";
+import { useRecoilState } from "recoil";
 import { atom } from "recoil";
+
+export default function CharacterCounter() {
+    return (
+        <div>
+            <TextInput />
+        </div>
+    );
+}
 
 const textState = atom({
     key: "textState",
     default: "",
 });
 
-export default function CharacterCounter() {
-    return <div></div>;
-}
+const TextInput = () => {
+    const [text, setText] = useRecoilState(textState);
+
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setText(e.target.value);
+    };
+
+    return (
+        <div style={{ display: "flex", justifyContent: "center", alignItems: "center", fontSize: "3rem" }}>
+            <div style={{ display: "flex", flexDirection: "column", width: "50%" }}>
+                <input type="text" value={text} onChange={handleChange} style={{ fontSize: "3rem" }} />
+                Echo: {text}
+            </div>
+        </div>
+    );
+};
