82817e67c78c25eb102830b7975f48e7a42c33e3
m19e <nexus.me7290@gmail.com>
2020-12-15 03:20:25 +0900

    Add text state

--- src/components/CharacterCounter.tsx
+++ src/components/CharacterCounter.tsx
@@ -1,4 +1,10 @@
 import React from "react";
+import { atom } from "recoil";
+
+const textState = atom({
+    key: "textState",
+    default: "",
+});
 
 export default function CharacterCounter() {
     return <div></div>;
