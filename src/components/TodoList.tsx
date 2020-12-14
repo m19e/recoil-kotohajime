8dc3625545bba16f48485945b7a283b73951caf1
m19e <nexus.me7290@gmail.com>
2020-12-15 04:39:12 +0900

    Add todolist input

---
+++ src/components/TodoList.tsx
@@ -0,0 +1,56 @@
+import React, { useState } from "react";
+import { atom, useSetRecoilState, useRecoilValue } from "recoil";
+
+type TodoItem = {
+    id: number;
+    text: string;
+    done: boolean;
+};
+
+const todoListState = atom<TodoItem[]>({
+    key: "todoListState",
+    default: [],
+});
+
+export default function TodoList() {
+    const todoList = useRecoilValue(todoListState);
+
+    return (
+        <div style={{ display: "flex", justifyContent: "center", alignItems: "center" }}>
+            <TodoItemCreator />
+        </div>
+    );
+}
+
+const TodoItemCreator = () => {
+    const [inputValue, setInputValue] = useState("");
+    const setTodoList = useSetRecoilState(todoListState);
+
+    const addItem = () => {
+        setTodoList((prev) => [
+            ...prev,
+            {
+                id: getId(),
+                text: inputValue,
+                done: false,
+            },
+        ]);
+        setInputValue("");
+    };
+
+    const handleChange = ({ target: { value } }: React.ChangeEvent<HTMLInputElement>) => {
+        setInputValue(value);
+    };
+
+    return (
+        <div>
+            <input type="text" value={inputValue} onChange={handleChange} style={{ fontSize: "3rem" }} />
+            <button onClick={addItem} style={{ fontSize: "3rem", color: "gray" }}>
+                Add
+            </button>
+        </div>
+    );
+};
+
+let id = 0;
+const getId = () => id++;
