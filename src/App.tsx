aaa398cee0cf8df7f79cb8f7228f3263cc277dcf
m19e <nexus.me7290@gmail.com>
2020-12-15 03:15:36 +0900

    Style app

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,16 @@
 import React from "react";
 import { RecoilRoot } from "recoil";
+import CharacterCounter from "./components/CharacterCounter";
 
 export default function App() {
-    return <RecoilRoot></RecoilRoot>;
+    return (
+        <RecoilRoot>
+            <div style={{ width: "100%", height: "100vh", backgroundColor: "#1DA1F2" }}>
+                <div style={{ display: "flex", flex: 1, flexDirection: "column" }}>
+                    <h1>Recoil</h1>
+                    <CharacterCounter />
+                </div>
+            </div>
+        </RecoilRoot>
+    );
 }
