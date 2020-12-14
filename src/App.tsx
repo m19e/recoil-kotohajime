d20f048e3873becac91ac807ee723e3866746dea
m19e <nexus.me7290@gmail.com>
2020-12-15 03:18:16 +0900

    Style h1

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ export default function App() {
         <RecoilRoot>
             <div style={{ width: "100%", height: "100vh", backgroundColor: "#1DA1F2" }}>
                 <div style={{ display: "flex", flex: 1, flexDirection: "column" }}>
-                    <h1>Recoil</h1>
+                    <h1 style={{ textAlign: "center", fontSize: "6rem", color: "white" }}>Recoil</h1>
                     <CharacterCounter />
                 </div>
             </div>
